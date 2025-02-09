85b71026f587c250b8da45a960e59fdfaf45847f
GuanzhenQian2004 <stevenqian2004@gmail.com>
2025-02-08 20:02:45 -0500

    Optimized database

--- backend/stockdata/models.py
+++ backend/stockdata/models.py
@@ -8,14 +8,11 @@ class StockData(models.Model):
     close_price = models.FloatField(null=True)
     volume = models.BigIntegerField(null=True)
     dividends = models.FloatField(null=True)
-    stock_splits = models.FloatField(null=True)
-    pct_change = models.FloatField(null=True)  # Percentage change for the close price
-    
-    # Additional fields for the new algorithm
-    free_cash_flow = models.FloatField(null=True)  # Yearly Free Cash Flow
-    eps = models.FloatField(null=True)             # Earnings per Share (EPS)
-    market_cap = models.FloatField(null=True)        # Market Capitalization = Volume * Close
-    pe = models.FloatField(null=True)                # Price-to-Earnings Ratio = Close / EPS
+    pct_change = models.FloatField(null=True)
+    free_cash_flow = models.FloatField(null=True)
+    eps = models.FloatField(null=True)       
+    market_cap = models.FloatField(null=True)        
+    pe = models.FloatField(null=True)               
 
     def __str__(self):
         return f"{self.timestamp} - Close: {self.close_price}"
